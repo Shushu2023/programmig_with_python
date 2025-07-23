ed2761e6b6d80ed6f199269eb207befb54c9f98b
snimeri <snimeri0@email.cpcc.edu>
2025-07-23 08:59:04 -0400

    fixed code and added comment

--- playback_speed.py
+++ playback_speed.py
@@ -1,2 +1,8 @@
-x= input("print what you want and input and I will output  that same input, replacing each space with ...").strip()
-print(f"{x}")
+'''
+Some people have a habit of lecturing speaking rather quickly, and it’d be nice to slow them down, a la YouTube’s 0.75 playback speed, or even by having them pause between words.
+
+In a file called playback.py, implement a program in Python that prompts the user for input and then outputs that same input, replacing each space with ... (i.e., three periods).
+'''
+user_input = input("enter your text here: ").strip()
+output = user_input.replace(" ","...")
+print(output)
